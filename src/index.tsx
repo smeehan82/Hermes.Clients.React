82af50e0efd3a1b79f14f8fabc76fdb13ba263ee
Andrew Grady <agrady42@gmail.com>
2016-06-25 12:05:43 -0400

    added placeholder routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ declare var ENVIRONMENT: 'development' | 'staging' | 'production';
 import * as React from 'react';
 import { render } from 'react-dom';
 import DevTools from 'mobx-react-devtools';
+import {useStrict} from 'mobx';
+import { Router, browserHistory, Route } from 'react-router';
+
+import routes from './routes';
+
+
+useStrict(true);
 
 const app = (
     <div>
-        <p>Hello, World</p>
+        <Router history={browserHistory} onUpdate={() => { window.scrollTo(0, 0); } } routes={routes} />
         {ENVIRONMENT === "development" ? <DevTools /> : null}
+        <DevTools />
     </div>
 );
 
