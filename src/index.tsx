4b00cb2e777fcd892190e6de5336c42df345f409
Andrew Grady <agrady42@gmail.com>
2016-06-25 10:35:57 -0400

    added mobx dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
+declare var ENVIRONMENT: 'development' | 'staging' | 'production';
 import * as React from 'react';
 import { render } from 'react-dom';
+import DevTools from 'mobx-react-devtools';
 
 const app = (
-    <div>Hello, World</div>
+    <div>
+        <p>Hello, World</p>
+        {ENVIRONMENT === "development" ? <DevTools /> : null}
+    </div>
 );
 
 render(app, document.getElementById('app'));
