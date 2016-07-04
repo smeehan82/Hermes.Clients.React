e78b359c62e5554864bb8d8a76b7ace92b3b8893
Steven Meehan <smeehan82@gmail.com>
2016-07-04 13:43:02 -0400

    added the getContentDetailsComponent function

--- src/Content/contentRoutes.tsx
+++ src/Content/contentRoutes.tsx
@@ -5,7 +5,9 @@ import {ContentList} from './ContentList';
 import ContentDetails from './ContentDetails';
 import ContentTypeList from './ContentTypeList';
 
-import {BookList} from './Books';
+import {BookList, BookDetails} from './Books';
+import {QuoteList, QuoteDetail} from './Quotes';
+import {MusicalInstrumentList, MusicalInstrumentDetails} from './MusicalInstruments';
 
 function getContentListComponent(
     nextState: ReactRouter.RouterState,
@@ -33,10 +35,36 @@ function getContentListComponent(
         }
 }
 
+function getContentDetailsComponent(
+    nextState: ReactRouter.RouterState,
+    cb: (error: any, component?: ReactRouter.RouteComponent) => void
+    ): void {
+
+        //@TODO: make api call to get code to get module for a given content type
+        switch (nextState.params['urlName']) {
+            case 'books':
+                cb(null, BookDetails);
+                break;
+            case 'quotes':
+                cb(null, QuoteDetail);
+                break;
+            case 'musical-instruments':
+                cb(null, MusicalInstrumentDetails);
+                break;
+            case 'temp':
+                //render default content component
+                cb('Not implemented yet');
+                break;
+            default:
+                cb('content type not found!');
+                break;
+        }
+}
+
 export default (
     <Route path='/content'>
         <IndexRoute component={ContentTypeList} />
         <Route path=':urlName' getComponent={getContentListComponent} />
-        <Route path=':urlName/:slug' component={ContentDetails} />
+        <Route path=':urlName/:slug' getComponent={getContentDetailsComponent} />
     </Route>
 );
