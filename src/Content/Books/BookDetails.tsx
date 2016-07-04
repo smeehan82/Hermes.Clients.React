771633d18971282b8a1e94b0e97b4f5aba2dfa19
Steven Meehan <smeehan82@gmail.com>
2016-07-04 13:39:54 -0400

    added the book details component

---
+++ src/Content/Books/BookDetails.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {observer} from 'mobx-react';
+import {Link} from 'react-router';
+
+import BookStore from './BookStore';
+
+interface IProps {
+    params: {
+        urlName: string;
+        slug: string;
+    };
+}
+
+@observer
+export class BookDetails extends React.Component<IProps, void>{
+    render(){
+        console.log('Book details rendering: %o', BookStore);
+        const book = BookStore.content.find(b=>b.slug === this.props.params.slug);
+
+        return (
+            <div>
+                <h2>{book.title}</h2>
+                <p>[Book Detail Component]</p>
+            </div>
+        );
+    }
+}
